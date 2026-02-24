0bab0b5e9632fa9d1f15683579dd2080bb092b49
Danilo Amorim <amorim.danilo@hotmail.com>
2026-02-24 19:40:35 -0300

    Heading e Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
+import { Container } from './components/Container';
+//import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu'
 
 import './styles/theme.css';
 import './styles/global.css';
-import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 
 export function App() {
     return <>
     <Container>
-        <Heading>Logo</Heading>
+        <Logo />
     </Container>
 
     <Container>
-        <Heading>Menu</Heading>
-    </Container>
-
-    <Container>
-        <Heading>Form</Heading>
-    </Container>
-
-    <Container>
-        <Heading>Footer</Heading>
+        <Menu />
     </Container>
     </>
 }
